16d9496ac03c2b2d629130185e86c859865138f3
RIcardoHerrera2004 <laboratorio@udla-ec.int>
2023-10-24 14:46:47 -0500

    Segunda version del programa: Se crea un bucle donde se evalue cada digito del numero ingresado por el usuario

--- Evaluacion.c
+++ Evaluacion.c
@@ -2,7 +2,21 @@
 
 int main (void) { //Se crea la funcion principal del programa//
 
-   
+    int numero, digito, sumatotal=0; //Se agrega las variables del programa//
+    
+    printf ("Ingrese un numero: "); //Se imprime la indicacion//
+    scanf ("%d", &numero); //Se lee el numero que va ingresar el usuario//
+    
+    do //Se utiliza el do while para crear un bucle de obtencion de digitos hasta enontrar todos los digitos del numero ingresado//
+    {
+        digito = numero%10; //Se utiliza el modulo para sacar el residuo de la division por 10, consiguiendo el digito del numero ingresado//
+        numero = numero/10; //Se divide el numero para 10 para seguir encontrando los digitios que faltan//
+        printf ("El primer digito es: %d \n", digito);
+        printf ("El numero es: %d \n", numero);
+
+    } while (numero>0);
+    
+
 
     return 0;
 }
