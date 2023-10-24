525d532681e55e345940a7bb5d558a49124203fc
RIcardoHerrera2004 <laboratorio@udla-ec.int>
2023-10-24 14:45:00 -0500

    Primera version del programa: Se crea las bases del codigo, agregando la libreria y escribiendo la funcion principal

--- Evaluacion.c
+++ Evaluacion.c
@@ -2,21 +2,7 @@
 
 int main (void) { //Se crea la funcion principal del programa//
 
-    int numero, digito, sumatotal=0; //Se agrega las variables del programa//
-    
-    printf ("Ingrese un numero: "); //Se imprime la indicacion//
-    scanf ("%d", &numero); //Se lee el numero que va ingresar el usuario//
-    
-    do //Se utiliza el do while para crear un bucle de obtencion de digitos hasta enontrar todos los digitos del numero ingresado//
-    {
-        digito = numero%10; //Se utiliza el modulo para sacar el residuo de la division por 10, consiguiendo el digito del numero ingresado//
-        numero = numero/10; //Se divide el numero para 10 para seguir encontrando los digitios que faltan//
-        printf ("El primer digito es: %d \n", digito);
-        printf ("El numero es: %d \n", numero);
-        sumatotal=digito+sumatotal;
-    } while (numero>0);
-    
-    printf ("La suma de los digitos del numero leido es: %d", sumatotal);
+   
 
     return 0;
 }
